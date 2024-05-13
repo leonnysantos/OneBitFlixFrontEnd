3feac2103c0bb7e70a2f619a9fa3f80111351221
leonnysantos <leonnypat@gmail.com>
2024-05-13 19:34:38 -0300

    Configuração inicial

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const HomeNotAuth = function () {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main></main>
+    </>
+  )
+};
+
+export default HomeNotAuth;
