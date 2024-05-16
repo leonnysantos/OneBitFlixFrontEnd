97c32ac71320e17db530ce9f4606d5bb733256c8
leonnysantos <leonnypat@gmail.com>
2024-05-15 21:51:41 -0300

    Criando página de login e fazendo as verificações

--- pages/register.tsx
+++ pages/register.tsx
@@ -39,8 +39,8 @@ const Register = function () {
 
         const { data, status } = await authService.register(params);
 
-        if (data.status === 201) {
-            router.push("/login?sucess=true");
+        if (status === 201) {
+            router.push("/login?registred=true");
         } else {
             setToastIsOpen(true);
             setToastMessage(data.message);
