b9d6204e5a84316a1c12c5eae3669ac9cb5d21e7
leonnysantos <leonnypat@gmail.com>
2024-05-20 22:55:51 -0300

    Criação do header logado, estilos e responsividade

--- pages/login.tsx
+++ pages/login.tsx
@@ -16,6 +16,12 @@ const Login = function () {
     const [toastMessage, setToastMessage] = useState("");
 
     useEffect(() => {
+        if (sessionStorage.getItem("onebitflix-token")) {
+            router.push("/home");
+        }
+    }, []);
+
+    useEffect(() => {
         const registerSuccess = router.query.registred;
 
         if (registerSuccess === "true") {
